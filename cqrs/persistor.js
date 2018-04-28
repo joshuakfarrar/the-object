987c6b2f4716ece7f0ddb82f5091f2ec69661067
Joshua K. Farrar <joshua@sent1nel.me>
2018-04-28 00:47:29 -0400

    convert tabs to spaces

--- cqrs/persistor.js
+++ cqrs/persistor.js
@@ -1,20 +1,20 @@
 'use strict';
 
 var Persistor = function(driver) {
-	return {
-		restore: function(aggregate, id) {
-			return driver.fetchEventsForAggregate(id)
-			  .then(events => {
-			    return aggregate.restore(events);
-			  });
-		},
-		save: function(aggregate) {
-			return driver.saveEvents(aggregate.getUnsavedEvents())
-				.then(success => {
-					return aggregate.onSave();
-				});
-		}
-	}
+  return {
+    restore: function(aggregate, id) {
+      return driver.fetchEventsForAggregate(id)
+        .then(events => {
+          return aggregate.restore(events);
+        });
+    },
+    save: function(aggregate) {
+      return driver.saveEvents(aggregate.getUnsavedEvents())
+        .then(success => {
+          return aggregate.onSave();
+        });
+    }
+  }
 }
 
 module.exports = Persistor;
