304f8b737e6623bc9f2fc03bc818f5fe17dd2772
Joshua K. Farrar <joshua@sent1nel.me>
2018-04-28 01:18:29 -0400

    abstract out event pre and post-processing from the knex driver (stringify and parse)

--- cqrs-driver-knex/index.js
+++ cqrs-driver-knex/index.js
@@ -2,27 +2,26 @@
 
 const _ = require('lodash');
 
+var prep = function(op) {
+  return function(events) {
+    return _.each(events, function(event) {
+      return _.merge(event, {
+        payload: op(event.payload)
+      });
+    })
+  }
+}
+
 var Driver = function(knex) {
   return require('cqrs').Driver({
     fetchEventsForAggregate: function(id) {
       return new Promise((resolve, reject) => {
-        return knex('events').where({ aggregateId: id })
-          .then(events => {
-            return resolve(_.each(events, function(event) {
-              return _.merge(event, {
-                payload: JSON.parse(event.payload)
-              });
-            }));
-          });
+        return resolve(knex('events').where({ aggregateId: id }).then(prep(JSON.parse)));
       });
     },
     saveEvents: function(events) {
       return new Promise((resolve, reject) => {
-        return resolve(knex('events').insert(_.each(events, function(event) {
-          return _.merge(event, {
-            payload: JSON.stringify(event.payload)
-          });
-        })));
+        return resolve(knex('events').insert(prep(JSON.stringify)(events)));
       });
     }
   });
